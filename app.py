c3760759d4aa9d576e9a8cff1e0eacaf91430bed
Sanskruti D <151908776+emergingsana123@users.noreply.github.com>
2024-04-30 01:15:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
- import streamlit as st
+import streamlit as st
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
 from reportlab.lib.pagesizes import letter
